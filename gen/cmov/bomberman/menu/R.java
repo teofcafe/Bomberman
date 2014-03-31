537f974b84b404219a1e724dccdcf3317c35bf4b
Teo <telmix_27@hotmail.com>
2014-03-31 17:27:04 +0100

    versao pausada

--- gen/cmov/bomberman/menu/R.java
+++ gen/cmov/bomberman/menu/R.java
@@ -37,32 +37,33 @@ public final class R {
         public static final int up=0x7f02000c;
     }
     public static final class id {
-        public static final int TextView01=0x7f080013;
-        public static final int TextView02=0x7f080015;
-        public static final int action_settings=0x7f080019;
-        public static final int bombButton=0x7f08000a;
-        public static final int button1=0x7f080004;
-        public static final int button2=0x7f080005;
-        public static final int cancelButton=0x7f080016;
-        public static final int changeNameButton=0x7f080011;
-        public static final int downButton=0x7f080008;
-        public static final int gridview=0x7f080018;
-        public static final int insertName=0x7f080014;
-        public static final int leftButton=0x7f080006;
-        public static final int linearLayout=0x7f08000b;
-        public static final int linearLayout1=0x7f08000c;
+        public static final int TextView01=0x7f080014;
+        public static final int TextView02=0x7f080016;
+        public static final int action_settings=0x7f08001a;
+        public static final int bombButton=0x7f08000b;
+        public static final int button1=0x7f080005;
+        public static final int button2=0x7f080006;
+        public static final int cancelButton=0x7f080017;
+        public static final int changeNameButton=0x7f080012;
+        public static final int downButton=0x7f080009;
+        public static final int gameBoard=0x7f080004;
+        public static final int gridview=0x7f080019;
+        public static final int insertName=0x7f080015;
+        public static final int leftButton=0x7f080007;
+        public static final int linearLayout=0x7f08000c;
+        public static final int linearLayout1=0x7f08000d;
         public static final int numberPlayersTextView=0x7f080003;
         public static final int playerNameTextView=0x7f080000;
         public static final int playerScoreTextView=0x7f080001;
-        public static final int quitButton=0x7f080012;
-        public static final int rightButton=0x7f080009;
-        public static final int saveButton=0x7f080017;
-        public static final int startGamebutton=0x7f080010;
+        public static final int quitButton=0x7f080013;
+        public static final int rightButton=0x7f08000a;
+        public static final int saveButton=0x7f080018;
+        public static final int startGamebutton=0x7f080011;
         public static final int timeLeftTextView=0x7f080002;
-        public static final int upButton=0x7f080007;
-        public static final int usernameHomePage=0x7f08000d;
-        public static final int welcomeMessageHomePagept1=0x7f08000e;
-        public static final int welcomeMessageHomePagept2=0x7f08000f;
+        public static final int upButton=0x7f080008;
+        public static final int usernameHomePage=0x7f08000e;
+        public static final int welcomeMessageHomePagept1=0x7f08000f;
+        public static final int welcomeMessageHomePagept2=0x7f080010;
     }
     public static final class layout {
         public static final int activity_game=0x7f030000;
