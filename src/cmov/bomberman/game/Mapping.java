3c6bcbff014ccfd26a4e88a1a256e6d656557a98
Tiago Francisco <tiago_francisco92@hotmail.com>
2014-04-21 00:36:53 +0100

    New translate, adapted to 30X30 images

--- src/cmov/bomberman/game/Mapping.java
+++ src/cmov/bomberman/game/Mapping.java
@@ -7,8 +7,14 @@ public class Mapping {
 	public static Pair mapToScreen(Pair coordinates,int screenWidth,int screenHeight, int fileWidth, int fileHeight){
 		int x = (Integer) coordinates.getKey();
 		int y = (Integer) coordinates.getValue();
-		int newX = (((x*2)+1)*10);
-		int newY = (((y*2)+1)*10);
+		//Para size = 20
+		
+		//int newX = (((x*2)+1)*10);
+		//int newY = (((y*2)+1)*10);
+		
+		//Para size = 30
+		int newX = ((x*30)+25);
+		int newY = ((y*30)+25);
 		Pair screen = new Pair(newX,newY);
 		return screen;
 	}
